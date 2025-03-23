f8fe3af0183ff58b5541647035df507afcd7198d
porkflossbun <chantalkozlo101@gmail.com>
2025-03-23 12:12:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -18,21 +18,21 @@
             <h3 class="text-center">LATEST ARTICLES</h3>
             <div class="row">
     <div class="col-md-4">
-    <img class="img-fluid" src="http://170.187.231.66/~mesh16/ wp-content/themes/starter_for_josh_site/images/game.jpg" alt="woman">
+    <img class="img-fluid" src="http://170.187.231.66/~mesh16/wp-content/themes/starter_for_josh_site/images/port1.jpg" alt="woman">
     <h4 class="article-title">A PORTRAIT</h4>
     <p class="date">Friday November 2030</p>
     <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
     <a class="readmore" href="#">CONTINUE READING</a>
     </div>
     <div class="col-md-4">
-    <img class="img-fluid" src="http://170.187.231.66/~mesh16/ wp-content/themes/starter_for_josh_site/images/game.jpg" alt="knights">
+    <img class="img-fluid" src="http://170.187.231.66/~mesh16/wp-content/themes/starter_for_josh_site/images/port2.jpg" alt="knights">
     <h4 class="article-title">A PORTRAIT</h4>
     <p class="date">Friday November 2030</p>
     <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
     <a class="readmore" href="#">CONTINUE READING</a>
     </div>
     <div class="col-md-4">
-    <img class="img-fluid" src="http://170.187.231.66/~mesh16/ wp-content/themes/starter_for_josh_site/images/game.jpg" alt="map">
+    <img class="img-fluid" src="http://170.187.231.66/~mesh16/wp-content/themes/starter_for_josh_site/images/port3.jpg" alt="map">
     <h4 class="article-title">A PORTRAIT</h4>
     <p class="date">Friday November 2030</p>
     <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
