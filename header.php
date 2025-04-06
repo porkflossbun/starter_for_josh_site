7723d00f7852021389acc43c1241a01c3591feea
porkflossbun <chantalkozlo101@gmail.com>
2025-04-06 20:54:09 +0800

    Update header.php

--- header.php
+++ header.php
@@ -56,7 +56,7 @@
 </head>
 <body>
 
-<header class="container-fluid bg1">
+<header class="container-fluid" style="background-size: cover; background-image: url(<?php the_field('headerbackgroundimage') ?>);">
     <div class="container">
     <nav class="navbar navbar-expand-md navbar-dark">
         <!-- Brand -->
@@ -88,7 +88,7 @@ wp_nav_menu( array(
 
 
       <div class="tag">
-      <h1>Game Design For the Future</h1>
+      <h1><?php the_field('maintagline'); ?></h1>
     </div>
 
     </div> <!-- container -->
