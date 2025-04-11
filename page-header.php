89a47614ee3aebb44f67423ee918ee540f0d7b77
porkflossbun <chantalkozlo101@gmail.com>
2025-04-11 16:19:03 +0800

    Update page-header.php

--- page-header.php
+++ page-header.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="<?php bloginfo('charset'); ?>"> 
+<meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="pls stop causing w3c errors" content="idk">
-
-
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,800;1,800&display=swap" rel="stylesheet">
-<title><?php
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet"> 
+
+<!--<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="responsive.css" />-->
+<title>   <?php
               if (function_exists('is_tag') && is_tag()) {
                  single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
               elseif (is_archive()) {
@@ -50,19 +52,18 @@
     } else {
         echo esc_attr(get_bloginfo('description'));
     }
-?>
-<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
-<meta name="description" content="<?php bloginfo('descrption'); ?>">
+?>">
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
+
 </head>
 <body>
-
-<header class="container-fluid smallpage-bg">
-    <div class="container">
-    <nav class="navbar navbar-expand-md navbar-dark">
+    <header class="container-fluid bg1">   <!-- div for the main image the stretches to the edge of the page- -->
+        <div class="container">
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
         <!-- Brand -->
-        <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange"> WHITKIN</span></a>
+        <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
       
-        <!-- Toggler/collapsibe Button -->
+        <!-- Toggler/collapsible Button -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -84,12 +85,16 @@
               </li>
           </ul>
         </div>
-      </nav>
+      </nav> 
+   
+        <div class="tag"><!-- div that holds the content in the middle of the page-->
+                  <h1 > Games Design For the Future </h1><!--  the main tag line-->
+  </div>            
+</div><!-- container-->
 
-      <div class="smallpage-tag">
-      <h1 class="smallpage-title">Projects</h1>
-    </div>
+<!—remove all our css styles and any Javascript code. WordPress automatically calls javascript /jquery elsewhere, calls to javascript are not needed in the header or the footer now- UNLESS you have calls to google or typkit fonts- they can stay-->
 
-    </div> <!-- container -->
-    <?php wp_head(); ?> 
-    </header>
+<!-- very important that this code is added-->
+<?php wp_head(); ?> 
+
+</header>
